a8855ca80a34bf489c85b0a87d451399b07c1bf6
jesperpi <jesper.pipping@gmail.com>
2026-05-18 19:34:41 +0200

    remove deprecated lspconfig

--- kickstart-python.lua
+++ kickstart-python.lua
@@ -64,26 +64,26 @@ local plugins = {
 		init = function()
 			-- this snippet enables auto-completion
 			local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
-      lspCapabilities.general.positionEncodings = { "utf-16" }
+                        lspCapabilities.general.positionEncodings = { "utf-16" }
 			lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
 
+
 			-- setup pyright with completion capabilities
-			require("lspconfig").pyright.setup({
-				capabilities = lspCapabilities,
-			})
-
+                        vim.lsp.config['pyright'] = {
+                          capabilities = lspCapabilities,
+                        }
 			-- setup taplo with completion capabilities
-			require("lspconfig").taplo.setup({
-				capabilities = lspCapabilities,
-			})
-
+                        vim.lsp.config['taplo'] = {
+                          capabilities = lspCapabilities,
+                        }
 			-- ruff uses an LSP proxy, therefore it needs to be enabled as if it
 			-- were a LSP. In practice, ruff only provides linter-like diagnostics
 			-- and some code actions, and is not a full LSP yet.
-			require("lspconfig").ruff.setup({
-				-- disable ruff as hover provider to avoid conflicts with pyright
-				on_attach = function(client) client.server_capabilities.hoverProvider = false end,
-			})
+                        vim.lsp.config['ruff'] = {
+			  -- disable ruff as hover provider to avoid conflicts with pyright
+                          on_attach = function(client) client.server_capabilities.hoverProvider = false end,
+                        }
+			-- setup taplo with completion capabilities
 		end,
 	},
 
